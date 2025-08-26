103f8570e11b3974f120f007039b72f4cde5bf4f
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-26 17:27:44 +0530

    Scholar details table migration added.

---
+++ database/migrations/2025_08_26_095557_create_scholar_details_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scholar_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('scholar_id')->references('id')->on('scholars');
+            $table->string('secondary_school_name')->nullable();
+            $table->string('secondary_school_year')->nullable();
+            $table->string('secondary_school_subjects')->nullable();
+            $table->string('secondary_school_aggregate')->nullable();
+            $table->string('secondary_school_grade')->nullable();
+            $table->string('secondary_school_board')->nullable();
+            $table->string('hs_school_name')->nullable();
+            $table->string('hs_school_year')->nullable();
+            $table->string('hs_school_board')->nullable();
+            $table->string('hs_school_subjects')->nullable();
+            $table->string('hs_school_aggregate')->nullable();
+            $table->string('hs_school_grade')->nullable();
+            $table->string('grad_course')->nullable();
+            $table->string('grad_pass_year')->nullable();
+            $table->string('grad_university')->nullable();
+            $table->string('grad_aggregate')->nullable();
+            $table->string('grad_subject')->nullable();
+            $table->string('grad_grade')->nullable();
+            $table->string('post_grad_course')->nullable();
+            $table->string('post_grad_pass_year')->nullable();
+            $table->string('post_grad_university')->nullable();
+            $table->string('post_grad_aggregrate')->nullable();
+            $table->string('post_grad_subject')->nullable();
+            $table->string('post_grad_grade')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('pincode')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scholar_details');
+    }
+};
