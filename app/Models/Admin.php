1e3415fd1269a936b7213f8ec28e3b0a46868c51
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-26 14:57:01 +0530

    Updated Admin model and migration.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +19,7 @@ class Admin extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'is_active',
         'password',
     ];
 
