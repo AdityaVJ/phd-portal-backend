4311dfa282dc8d9685d09b52500765d23a7b6909
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-26 18:13:36 +0530

    Added the HasApiTokens trait to Admin model.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
