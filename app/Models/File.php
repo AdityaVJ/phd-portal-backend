9d82e3109a01b7d2f3efe160a170ade329847bd5
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-26 17:31:30 +0530

    File model and migration created.

---
+++ app/Models/File.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class File extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'url',
+        'display_name',
+        'description',
+        'tag',
+        'fileable_id',
+        'fileable_type',
+    ];
+}
