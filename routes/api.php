7fa46c93a3ca0d8983c177fbf09493630df637d7
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-27 18:22:37 +0530

    Middleware name updated for admin routes.

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     Route::post('/login', [AdminAuthController::class, 'login']);
     Route::post('/refresh', [AdminAuthController::class, 'refresh']);
-    Route::middleware('auth:sanctum,admin')->group(function () {
+    Route::middleware('auth:admin')->group(function () {
         Route::post('/logout', [AdminAuthController::class, 'logout']);
         Route::post('/logout-all', [AdminAuthController::class, 'logoutAll']);
         Route::get('/sessions', [AdminAuthController::class, 'sessions']);
