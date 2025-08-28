4171264f18d8a1a1bebf5fbe392c350fedf29a23
Ayushi Kamthan <kamthanayushi102@gmail.com>
2025-08-28 18:48:00 +0530

    Extended scholar login function with user object and added HasApiToken.

--- app/Models/Scholar.php
+++ app/Models/Scholar.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Scholar extends Authenticatable
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
