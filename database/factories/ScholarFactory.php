94047f650f6b215ec26a622b611bc6512dc019d0
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2025-08-29 19:21:00 +0530

    ScholarFactory updated to update scholar_details table.

--- database/factories/ScholarFactory.php
+++ database/factories/ScholarFactory.php
@@ -33,4 +33,41 @@ class ScholarFactory extends Factory
             'date_of_birth' => $this->faker->date(),
         ];
     }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (\App\Models\Scholar $scholar) {
+            $scholar->saveDetails([
+                'secondary_school_name' => fake()->company,
+                'secondary_school_year' => fake()->year,
+                'secondary_school_subjects' => fake()->words(3, true),
+                'secondary_school_aggregate' => fake()->randomFloat(2, 50, 100),
+                'secondary_school_grade' => fake()->randomElement(['A', 'B', 'C']),
+                'secondary_school_board' => fake()->company,
+                'hs_school_name' => fake()->company,
+                'hs_school_year' => fake()->year,
+                'hs_school_board' => fake()->company,
+                'hs_school_subjects' => fake()->words(3, true),
+                'hs_school_aggregate' => fake()->randomFloat(2, 50, 100),
+                'hs_school_grade' => fake()->randomElement(['A', 'B', 'C']),
+                'grad_course' => fake()->word,
+                'grad_pass_year' => fake()->year,
+                'grad_university' => fake()->company,
+                'grad_aggregate' => fake()->randomFloat(2, 50, 100),
+                'grad_subject' => fake()->word,
+                'grad_grade' => fake()->randomElement(['A', 'B', 'C']),
+                'post_grad_course' => fake()->word,
+                'post_grad_pass_year' => fake()->year,
+                'post_grad_university' => fake()->company,
+                'post_grad_aggregrate' => fake()->randomFloat(2, 50, 100),
+                'post_grad_subject' => fake()->word,
+                'post_grad_grade' => fake()->randomElement(['A', 'B', 'C']),
+                'address' => fake()->address,
+                'city' => fake()->city,
+                'state' => fake()->state,
+                'pincode' => fake()->postcode,
+            ]);
+        });
+    }
+
 }
